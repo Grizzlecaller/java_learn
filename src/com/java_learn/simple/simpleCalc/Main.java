7042393e0b8aba16258e8a773ad772598bcee455
Grizzlecaller <dylebedev.other@gmail.com>
2022-03-24 22:11:47 +0300

    rename + fixes

--- src/com/java_learn/simple/simpleCalc/Main.java
+++ src/com/java_learn/simple/simpleCalc/Main.java
@@ -4,24 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner userInput = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
         System.out.print("input first number: ");
-        float num1 = userInput.nextInt();
+        calculator.setFirstNumber(userInput.nextInt());
 
         System.out.print("input second number: ");
-        float num2 = userInput.nextInt();
+        calculator.setSecondNumber(userInput.nextInt());
 
         System.out.print("input math action: ");
         String action = userInput.nextLine();
         action = userInput.nextLine();
-
-        SimpleCalcClass calculator;
-        calculator = new SimpleCalcClass();
-
-        calculator.setFirstNumber(num1);
-        calculator.setSecondNumber(num2);
         calculator.setAction(action);
+
         calculator.calculate();
     }
 }
